23bbc17909f30dbe4c81f10b95d8abffdbddb203
joelgrondrup <joel@joel.biz>
2025-12-09 09:40:55 +0100

    Changes to Microbizz application

--- src/Model/DataObjects/MicrobizzApplication.php
+++ src/Model/DataObjects/MicrobizzApplication.php
@@ -97,7 +97,7 @@ namespace {
 
             $activateHooksAndInterfacesLink = new CustomLink('activateWebhooksAndInterfaces','Activate hooks and interfaces');
             $activateHooksAndInterfacesLink->setButtonIcon(SilverStripeIcons::ICON_EXPORT);
-            $activateHooksAndInterfacesLink->setNewWindow(false);
+            $activateHooksAndInterfacesLink->setNewWindow(true);
 
             $actions->push($activateHooksAndInterfacesLink);
 
